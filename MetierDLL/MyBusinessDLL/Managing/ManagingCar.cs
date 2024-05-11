0f256fda38091b20c420c3bb2c2f91d1614fab09
Islem Mellehi <77405900+Islemmellehi@users.noreply.github.com>
2024-05-11 20:21:44 +0100

    Add files via upload

---
+++ MetierDLL/MyBusinessDLL/Managing/ManagingCar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using MyEntitiesDLL.Entities;
+
+namespace MyBusinessDLL.Managing
+{
+    public class ManagingCar
+    {
+        private readonly InformationAzure azureInfo;
+
+        public ManagingCar(InformationAzure azureInfo)
+        {
+            this.azureInfo = azureInfo;
+        }
+
+        public async Task SendPosition(Vehicule vehicle)
+        {
+
+            int i = 0;
+
+            while (true)
+            {
+                await using (var producerClient = new EventHubProducerClient(azureInfo.connectionString, azureInfo.eventHubName))
+                {
+                    using (EventDataBatch eventBatch = await producerClient.CreateBatchAsync())
+                    {
+                        eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Position :" + i + "P" + i)));
+                        i++;
+                        await producerClient.SendAsync(eventBatch);
+                    }
+                }
+
+                Console.WriteLine($"Sending position for Vehicle {vehicle.matricule}: Position {i}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ MetierDLL/MyBusinessDLL/Metier/GererVehicule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyEntitiesDLL.Entities;
+using System.Threading.Tasks;
+using MyBusinessDLL.Managing;
+using Newtonsoft.Json;
+
+
+namespace MyBusinessDLL.Metier
+{
+    public class GererVehicule
+    {
+        private Dictionary<string, Thread> vehicleThreads;
+        private readonly InformationAzure azureInfo;
+
+        public GererVehicule(InformationAzure info)
+        {
+            azureInfo = info ?? throw new ArgumentNullException(nameof(info));
+            vehicleThreads = new Dictionary<string, Thread>(); // Ensure vehicleThreads is initialized
+        }
+
+
+        public void DemarrerVehicule(Vehicule vehicle)
+        {
+            if (vehicle == null)
+                throw new ArgumentNullException(nameof(vehicle));
+
+            var managingCar = new ManagingCar(azureInfo);
+            Thread thread = new Thread(() => managingCar.SendPosition(vehicle));
+
+            vehicleThreads[vehicle.matricule] = thread;
+            thread.Start();
+        }
+
+        public void SuivreVehicule()
+        {
+
+        }
+        public void SeConnecterIOTHub(InformationAzure info)
+        {
+
+        }
+
+
+
+    }
+}
